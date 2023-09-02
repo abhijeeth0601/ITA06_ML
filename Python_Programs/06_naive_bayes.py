5a6428ab79a43d762706d315cd2d0f09df07b32b
ABHIJEETH <113879749+abhijeeth0601@users.noreply.github.com>
2023-09-02 20:10:00 +0530

    Update 06_naive_bayes.py

--- Python_Programs/06_naive_bayes.py
+++ Python_Programs/06_naive_bayes.py
@@ -1,55 +1,24 @@
+Navie Bayes
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, precision_score, f1_score
+from sklearn.naive_bayes import GaussianNB 
+from sklearn.metrics import accuracy score, precision_score, fi_score
 
-# Load the data from the CSV file
+lines pd.read_csv('D:/folders/ML/CSV_Files/naivedata.csv')
 
+x=lines.iloc[:, -1] 
+y= lines.iloc[:, -1]
 
-def load_data(lines):
-    lines = pd.read_csv('D:/folders/ML/CSV/naivedata.csv')
-    return lines
+x_train, x_test, y_train, y_test train_test_split( x, y, test_size-0.2,random_state=42)
+gnb.GaussianNB()
+gnb.fit(x_train, y_train) 
+y_pred=gnb.predict(x_test)
 
-# Split the data into features and target
-
-
-def split_data(df):
-    X = df.iloc[:, :-1]
-    y = df.iloc[:, -1]
-    return X, y
-
-# Train and test the Naive Bayes classifier
-
-
-def train_test_naive_bayes(X_train, y_train, X_test):
-    gnb = GaussianNB()
-    gnb.fit(X_train, y_train)
-    return gnb.predict(X_test)
-
-
-if __name__ == "__main__":
-    # Load data from CSV file
-    file_path = 'data.csv'
-    data = load_data(file_path)
-
-    # Split data into features and target
-    X, y = split_data(data)
-
-    # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42)
-
-    # Train and test the Naive Bayes classifier
-    y_pred = train_test_naive_bayes(X_train, y_train, X_test)
-
-    # Calculate and display accuracy
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Accuracy:", accuracy)
-
-    # Calculate and display precision
-    precision = precision_score(y_test, y_pred)
-    print("Precision:", precision)
-
-    # Calculate and display F1 score
-    f1 = f1_score(y_test, y_pred)
-    print("F1 Score:", f1)
+accuracy = accuracy_score (y_test, y_pred)
+print("Accuracy:", accuracy)
+#Calculate and display precision
+precision = precision_score (y_test, y_pred) print("Precision:", precision)
+# Calculate and display F1 score 
+fi = fi score(y_test, y_pred)
+print("F1 Score:", f1)
